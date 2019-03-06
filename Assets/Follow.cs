1a953294eadeae6407ea5593ac2a20e735b143a7
Lily Young <licyoung@ucsc.edu>
2019-03-06 01:00:14 -0800

    first check in fix

---
+++ Assets/Follow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Follow : MonoBehaviour
+{
+    public GameObject playerCharacter;
+    private Vector3 distance;
+    public float speedH = 2.0f;
+    public float speedV = 2.0f;
+
+    private float ex = 0.0f;
+    private float wai = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        distance = transform.position - playerCharacter.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = playerCharacter.transform.position + distance;
+        ex += speedH * Input.GetAxis("Mouse X");
+        wai -= speedV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(wai, ex, 0.0f);
+    }
+}
