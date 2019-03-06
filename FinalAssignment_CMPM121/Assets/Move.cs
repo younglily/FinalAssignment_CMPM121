a74c32bc2e026fd469dcba35ec7f9c6a699818fa
Lily Young <licyoung@ucsc.edu>
2019-03-06 00:30:54 -0800

    First check-in

---
+++ FinalAssignment_CMPM121/Assets/Move.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+    private float speed;
+    private CharacterController character;
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0, moveVertical);
+
+        character.Move(movement);
+        transform.rotation = Quaternion.LookRotation(movement);
+        transform.Translate(movement * speed * Time.deltaTime, Space.World);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (null != anim)
+            {
+                anim.Play("run");
+            }
+        }
+    }
+}
